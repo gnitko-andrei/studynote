70e4d66d0da500be317e0203a38db9121f32418f
Андрей Гнитько <gnitkoandrei17@gmail.com>
2019-12-03 21:57:08 +0300

    dev: add Repository and Controller(GET method) for User

---
+++ src/main/java/com/gnitko/studynote/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.gnitko.studynote.controller;
+
+import com.gnitko.studynote.entity.User;
+import com.gnitko.studynote.repo.UserRepo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserRepo userRepo;
+
+    public UserController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    @GetMapping("/users/{userId}")
+    public User getAllUsers(@PathVariable Long userId) {
+        return userRepo.findById(userId).get();
+    }
+}
