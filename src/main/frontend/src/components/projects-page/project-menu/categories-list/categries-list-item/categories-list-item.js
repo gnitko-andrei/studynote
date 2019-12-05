8813599adb6361e9e6317d790c3ec407324e928c
Андрей Гнитько <gnitkoandrei17@gmail.com>
2019-12-05 18:47:33 +0300

    front: add collapse for categoriesListItem

--- src/main/frontend/src/components/projects-page/project-menu/categories-list/categries-list-item/categories-list-item.js
+++ src/main/frontend/src/components/projects-page/project-menu/categories-list/categries-list-item/categories-list-item.js
@@ -6,7 +6,10 @@ export default class CategoriesListItem extends Component {
     render() {
         return (
             <>
-                <a href="#" className="list-group-item list-group-item-action">{this.props.categoryName}
+                <a href="#" className="list-group-item list-group-item-action"
+                data-toggle="collapse" href={'#' + this.props.categoryName} role="button"
+                   aria-expanded="false" aria-controls={this.props.categoryName}>
+                    {this.props.categoryName}
                     <button type="button"
                             className="btn btn-outline-danger btn-sm float-right mx-1">
                         <i className="fa fa-trash-o"></i>
@@ -16,6 +19,13 @@ export default class CategoriesListItem extends Component {
                         <i className="fa fa-edit"></i>
                     </button>
                 </a>
+                <div className="collapse" id={this.props.categoryName}>
+                    <div className="card card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                        Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                        proident.
+                    </div>
+                </div>
             </>
         )
     }
