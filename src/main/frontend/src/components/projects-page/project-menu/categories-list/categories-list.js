ffda99cb280f40a10fb805c257368cd6f534ad4a
Андрей Гнитько <gnitkoandrei17@gmail.com>
2019-12-06 09:44:56 +0300

    front: add modals for addCategoryButton and addNotesButton

--- src/main/frontend/src/components/projects-page/project-menu/categories-list/categories-list.js
+++ src/main/frontend/src/components/projects-page/project-menu/categories-list/categories-list.js
@@ -6,21 +6,52 @@ import CategoriesListItem from "./categries-list-item";
 export default class CategoriesList extends Component {
     render() {
         return (
-            <div className="accordion" id="categories-accordion">
-                <div className="list-group">
-                    <div className="list-group-item list-group-item-action active">
-                        Категории
-                        <button type="button"
-                                className="btn btn-success btn-sm float-right">
-                            <i className="fa fa-plus"></i>
-                        </button>
+            <>
+                <div className="accordion" id="categories-accordion">
+                    <div className="list-group">
+                        <div className="list-group-item list-group-item-action active">
+                            Категории
+                            <button type="button"
+                                    className="btn btn-success btn-sm float-right" data-toggle="modal"
+                                    data-target="#newCategoryModal">
+                                <i className="fa fa-plus"></i>
+                            </button>
+                        </div>
+                        <CategoriesListItem categoryName='Книги'/>
+                        <CategoriesListItem categoryName='Туториалы'/>
+                        <CategoriesListItem categoryName='Видео'/>
+                        <CategoriesListItem categoryName='Ссылки'/>
                     </div>
-                    <CategoriesListItem categoryName='Книги'/>
-                    <CategoriesListItem categoryName='Туториалы'/>
-                    <CategoriesListItem categoryName='Видео'/>
-                    <CategoriesListItem categoryName='Ссылки'/>
                 </div>
-            </div>
+
+                <div className="modal fade" id="newCategoryModal" tabIndex="-1" role="dialog"
+                     aria-labelledby="newCategoryModalTitle" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="newCategoryModalTitle">Новая категория</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form>
+                                <div className="modal-body">
+
+                                    <div className="form-group">
+                                        <label htmlFor="projectName">Название</label>
+                                        <input type="text" className="form-control" id="projectName"/>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="submit" className="btn btn-primary">Создать</button>
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Закрыть
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
--- src/main/frontend/src/components/projects-page/project-menu/categories-list/categries-list-item/notes-list/notes-list.js
+++ src/main/frontend/src/components/projects-page/project-menu/categories-list/categries-list-item/notes-list/notes-list.js
@@ -17,7 +17,8 @@ export default class NotesList extends Component {
                     <div className="list-group-item list-group-item-action active">
 
                         <button type="button"
-                                className="btn btn-success btn-sm float-right">
+                                className="btn btn-success btn-sm float-right" data-toggle="modal"
+                                data-target="#newNoteModal">
                             <i className="fa fa-plus"></i>
                         </button>
                     </div>
@@ -27,6 +28,42 @@ export default class NotesList extends Component {
                     <NotesListItem noteName={'Note4'}/>
                     <NotesListItem noteName={'Note5'}/>
                     <NotesListItem noteName={'Note6'}/>
+                </div>
+
+                <div className="modal fade" id="newNoteModal" tabIndex="-1" role="dialog"
+                     aria-labelledby="newNoteModalTitle" aria-hidden="true">
+                    <div className="modal-dialog modal-lg" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="newNoteModalTitle">Новая заметка</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form>
+                                <div className="modal-body">
+
+                                    <div className="form-group">
+                                        <label htmlFor="projectName">Название</label>
+                                        <input type="text" className="form-control" id="projectName"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="projectName">Ссылка</label>
+                                        <input type="text" className="form-control" id="projectName"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="projectDescription">Описание</label>
+                                        <textarea className="form-control" id="projectDescription" rows="5"></textarea>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="submit" className="btn btn-primary">Создать</button>
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Закрыть
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
